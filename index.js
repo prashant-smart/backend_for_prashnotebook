6ff73848dd7351578f38f6078f4cc3e1d43ecbcf
prashant-smart <72729279+prashant-smart@users.noreply.github.com>
2021-11-20 12:08:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 var cors = require('cors');
 
 const app = express()
-const port = process.env.PORT ||5000;// here we cant use 3000 because at that react app will be running
+const port = process.env.PORT;// here we cant use 3000 because at that react app will be running
 
 app.use(cors());
 
