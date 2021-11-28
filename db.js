36ec2350e503b1e701e6d0f8333a0ef872fb58f4
Prashant Kumar Yadav <kartik224blue@gmail.com>
2021-11-29 01:24:56 +0530

    host mongodb on heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const mongoURI="mongodb://localhost:27017/prashNoteBook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"  // in this we write prashNoteBook by own to create database of name prashNoteBook otherwise by default it store in test
 const connectToMongo=()=>{
-    mongoose.connect(mongoURI,()=>{
+    mongoose.connect(process.env.MONGODB_URI,()=>{
         console.log("connectToMongo.Sucessfully!");
     })
 }
